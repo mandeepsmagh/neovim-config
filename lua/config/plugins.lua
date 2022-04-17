8d2b1819d61df4f7d1d6a91e6967e6d9fe010f28
Mandeep Smagh <mandeep.smagh@synergy.net.au>
2022-04-17 18:10:45 +0800

    Included cmp plugins

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -18,6 +18,13 @@ end
 	 {'sakhnik/nvim-gdb'},
 	 {'nvim-lualine/lualine.nvim'},
 	 {'numToStr/Comment.nvim'},
+	 -- cmp plugins
+	 {'hrsh7th/nvim-cmp'}, -- The completion plugin
+	 {'hrsh7th/cmp-buffer'}, -- buffer completions
+	 {'hrsh7th/cmp-path'}, -- path completions
+	 {'hrsh7th/cmp-cmdline'}, -- cmdline completions
+	 {'saadparwaiz1/cmp_luasnip'}, -- snippet completions
+	 {'hrsh7th/cmp-nvim-lsp'},
 	-- LSP
 	{'neovim/nvim-lspconfig'},          -- enable LSP
 	{'williamboman/nvim-lsp-installer'}, -- simple to use language server installer
