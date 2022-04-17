22ca9dab009542a86a658b95a806b4e98a04682b
Mandeep Smagh <mandeep.smagh@synergy.net.au>
2022-04-17 17:40:32 +0800

    updated plugins module with new format

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -1,24 +1,26 @@
-local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
+local paq_dir = fmt('%s/site/pack/paqs/start/paq-nvim', vim.fn.stdpath('data'))
+
+if vim.fn.empty(vim.fn.glob(paq_dir)) > 0 then
+  vim.fn.system {'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', paq_dir}
+end
 
 -------------------- PLUGINS -------------------------------
-cmd 'packadd paq-nvim'               -- load the package manager
-local paq = require('paq')  -- a convenient alias
-paq {
-	'isavq/paq-nvim';  -- paq-nvim manages itself
-	'nvim-treesitter/nvim-treesitter';
-	'nvim-telescope/telescope.nvim';
-	'nvim-lua/plenary.nvim';
-	'nvim-lua/popup.nvim';
-	'kyazdani42/nvim-tree.lua';
-	'kyazdani42/nvim-web-devicons';
-	'navarasu/onedark.nvim';
-	'sakhnik/nvim-gdb';
-	'nvim-lualine/lualine.nvim';
-	'numToStr/Comment.nvim';
+ require'paq'{
+	 {'isavq/paq-nvim'},  -- paq-nvim manages itself
+	 {'nvim-treesitter/nvim-treesitter'},
+	 {'nvim-telescope/telescope.nvim'},
+	 {'nvim-lua/plenary.nvim'},
+	 {'nvim-lua/popup.nvim'},
+	 {'kyazdani42/nvim-tree.lua'},
+	 {'kyazdani42/nvim-web-devicons'},
+	 {'navarasu/onedark.nvim'},
+	 {'sakhnik/nvim-gdb'},
+	 {'nvim-lualine/lualine.nvim'},
+	 {'numToStr/Comment.nvim'},
 	-- LSP
-	'neovim/nvim-lspconfig';          -- enable LSP
-	'williamboman/nvim-lsp-installer'; -- simple to use language server installer
-	'tamago324/nlsp-settings.nvim';    -- language server settings defined in json for
-	'jose-elias-alvarez/null-ls.nvim'; -- for formatters and linters
+	{'neovim/nvim-lspconfig'},          -- enable LSP
+	{'williamboman/nvim-lsp-installer'}, -- simple to use language server installer
+	{'tamago324/nlsp-settings.nvim'},    -- language server settings defined in json for
+	{'jose-elias-alvarez/null-ls.nvim'} -- for formatters and linters
 }
 
