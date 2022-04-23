690c9c567f2c56c278b67c7ca45eb83cf154c54e
Mandeep Smagh <mandeep.smagh@synergy.net.au>
2022-04-23 08:23:25 +0800

    Included debuggin plugins

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -33,6 +33,11 @@ end
 	{'neovim/nvim-lspconfig'},          -- enable LSP
 	{'williamboman/nvim-lsp-installer'}, -- simple to use language server installer
 	{'tamago324/nlsp-settings.nvim'},    -- language server settings defined in json for
-	{'jose-elias-alvarez/null-ls.nvim'} -- for formatters and linters
+	{'jose-elias-alvarez/null-ls.nvim'}, -- for formatters and linters
+    -- debugging
+    {'mfussenegger/nvim-dap'},
+    {'rcarriga/nvim-dap-ui'},
+    {'theHamsta/nvim-dap-virtual-text'},
+    {'nvim-telescope/telescope-dap.nvim'}
 }
 
