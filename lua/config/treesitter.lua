77b55622b54f99d4cebfd42a721624ace353cf66
Mandeep Smagh <mandeep.smagh@synergy.net.au>
2022-04-25 17:38:44 +0800

    updated tree-sitter config

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,23 +1,36 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-  return
+    return
 end
 
 configs.setup {
-  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = { "" }, -- List of parsers to ignore installing
-  autopairs = {
-    enable = true,
-  },
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
-  },
-  indent = { enable = true, disable = { "yaml" } },
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
+    ensure_installed = { "lua", "rust", "javascript", "c_sharp" },
+    sync_install = false,
+    ignore_install = {},
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false
+    },
+    context_commentstring = {
+        enable = true,
+        enable_autocmd = false
+    },
+    autopairs = {
+        enable = true
+    },
+    incremental_selection = {
+        enable = true
+    },
+    indent = {
+        enable = false
+    },
+    rainbow = {
+        enable = true,
+        disable = {"html"},
+        extended_mode = false,
+        max_file_lines = nil
+    },
+    autotag = {
+        enable = true
+    }
 }
