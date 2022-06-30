dec50506ba582578d1a3d867ac979d813604e5d5
Mandeep Smagh <mandeep.smagh@synergy.net.au>
2022-06-30 16:37:17 +0800

    indented null-ls module

--- lua/config/lsp/null-ls.lua
+++ lua/config/lsp/null-ls.lua
@@ -9,15 +9,15 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup {
-  debug = false,
-  sources = {
-    formatting.prettierd.with {
-      extra_filetypes = { "toml" },
-      extra_args = { "--single-quote", "--jsx-single-quote" },
+    debug = false,
+    sources = {
+        formatting.prettierd.with {
+            extra_filetypes = { "toml" },
+            extra_args = { "--single-quote", "--jsx-single-quote" },
+        },
+        -- formatting.black.with { extra_args = { "--fast" } },
+        -- formatting.stylua,
+        -- formatting.google_java_format,
+        diagnostics.eslint_d
     },
-    -- formatting.black.with { extra_args = { "--fast" } },
-    -- formatting.stylua,
-    -- formatting.google_java_format,
-    diagnostics.eslint_d
-  },
 }
