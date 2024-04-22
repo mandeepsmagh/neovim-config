536e791a48775fd1de6a3df27dd2f8a231826d37
Mandeep Smagh <mandeep.smagh@outlook.com>
2024-04-22 11:47:53 +0800

    using prettier and eslint

--- lua/config/lsp/null-ls.lua
+++ lua/config/lsp/null-ls.lua
@@ -11,13 +11,13 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup {
     debug = false,
     sources = {
-        formatting.prettierd.with {
+        formatting.prettier.with {
             extra_filetypes = { "toml" },
             extra_args = { "--single-quote", "--jsx-single-quote" },
         },
         -- formatting.black.with { extra_args = { "--fast" } },
         -- formatting.stylua,
         -- formatting.google_java_format,
-        diagnostics.eslint_d
+        diagnostics.eslint
     },
 }
