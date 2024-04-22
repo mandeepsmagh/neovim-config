d997bcc11fbf00067fb465846a64ff9fbe9f7a33
Mandeep Smagh <mandeep.smagh@outlook.com>
2024-04-22 11:32:35 +0800

    updated plugin to start lazy

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -18,55 +18,171 @@ local lazy = require('lazy')
 
 -- Define your plugins
 local plugins = {
-    'nvim-treesitter/nvim-treesitter',
-    'nvim-telescope/telescope.nvim',
-    'nvim-lua/plenary.nvim',
-    'nvim-lua/popup.nvim',
+    {
+        'nvim-treesitter/nvim-treesitter',
+        lazy = true
+    },
+    {
+        'nvim-telescope/telescope.nvim',
+        lazy = true
+    },
+    {
+        'nvim-lua/plenary.nvim',
+        lazy = true
+    },
+    {
+        'nvim-lua/popup.nvim',
+        lazy = true
+    },
+    
     {
         'nvim-tree/nvim-tree.lua',
         lazy = true,
         opts = { on_attach = on_attach_change }
     },
-    'nvim-tree/nvim-web-devicons',
-    'akinsho/bufferline.nvim',
-    'navarasu/onedark.nvim',
-    'EdenEast/nightfox.nvim',
-    'sakhnik/nvim-gdb',
-    'nvim-lualine/lualine.nvim',
-    'numToStr/Comment.nvim',
-    'JoosepAlviste/nvim-ts-context-commentstring',
-    'nvim-orgmode/orgmode',
-    'tpope/vim-fugitive',
-    'akinsho/toggleterm.nvim',
-    'b0o/SchemaStore.nvim',
-    'windwp/nvim-autopairs', -- Autopairs, integrates with both cmp and treesitter
-    'goolord/alpha-nvim',
-    'RRethy/vim-illuminate', -- highlight selected
-    'lukas-reineke/indent-blankline.nvim',
+    {
+        'nvim-tree/nvim-web-devicons',
+        lazy = true
+    },
+    {
+        'akinsho/bufferline.nvim',
+        lazy = true
+    },
+    {
+        'navarasu/onedark.nvim',
+        lazy = true
+    },
+    {
+        'EdenEast/nightfox.nvim',
+        lazy = true
+    },
+    {
+        'sakhnik/nvim-gdb',
+        lazy = true
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        lazy = true
+    },
+    {
+        'numToStr/Comment.nvim',
+        lazy = true
+    },
+    {
+        'JoosepAlviste/nvim-ts-context-commentstring',
+    },
+    {
+        'nvim-orgmode/orgmode',
+        lazy = true
+    },
+    {
+        'tpope/vim-fugitive',
+        lazy = true
+    },
+    {
+        'akinsho/toggleterm.nvim',
+        lazy = true
+    },
+    {
+        'b0o/SchemaStore.nvim',
+        lazy = true
+    },
+    {
+        'windwp/nvim-autopairs', -- Autopairs, integrates with both cmp and treesitter
+        lazy = true
+    },
+    {
+        'goolord/alpha-nvim',
+        lazy = true
+    },
+    {
+        'RRethy/vim-illuminate', -- highlight selected
+        lazy = true
+    },
+    {
+        'lukas-reineke/indent-blankline.nvim',
+        lazy = true
+    },
     -- cmp plugins
-    'hrsh7th/nvim-cmp', -- The completion plugin
-    'hrsh7th/cmp-buffer', -- buffer completions
-    'hrsh7th/cmp-path', -- path completions
-    'hrsh7th/cmp-cmdline', -- cmdline completions
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-nvim-lua',
+    {
+        'hrsh7th/nvim-cmp', -- The completion plugin
+        lazy = true
+    },
+    {
+        'hrsh7th/cmp-buffer', -- buffer completions
+        lazy = true
+    },
+    {
+        'hrsh7th/cmp-path', -- path completions
+        lazy = true
+    },
+    {
+        'hrsh7th/cmp-cmdline', -- cmdline completions
+        lazy = true
+    },
+    {
+        'hrsh7th/cmp-nvim-lsp',
+        lazy = true
+    },
+    {
+        'hrsh7th/cmp-nvim-lua',
+        lazy = true
+    },
     -- snippets
-    'L3MON4D3/LuaSnip', -- snippet completions
-    'saadparwaiz1/cmp_luasnip', -- used together with above plugin
-    'rafamadriz/friendly-snippets', -- some extra snippets
+    {
+        'L3MON4D3/LuaSnip', -- snippet completions
+        lazy = true
+
+    },
+    {
+        'saadparwaiz1/cmp_luasnip', -- used together with above plugin
+        lazy = true
+    },
+    {
+        'rafamadriz/friendly-snippets', -- some extra snippets
+        lazy = true
+    },
     -- LSP
-    'neovim/nvim-lspconfig', -- enable LSP
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim', -- simple to use language server installer
-    'tamago324/nlsp-settings.nvim', -- language server settings defined in json for
-    'jose-elias-alvarez/null-ls.nvim', -- for formatters and linters
-    --Java Debugging plugin---
-    'mfussenegger/nvim-jdtls',
+    {
+        'neovim/nvim-lspconfig', -- enable LSP
+        lazy = true
+    },
+    {
+        'williamboman/mason.nvim',
+    },
+    {
+        'williamboman/mason-lspconfig.nvim', -- simple to use language server installer
+    },
+    {
+        'tamago324/nlsp-settings.nvim', -- language server settings defined in json for
+        lazy = true
+    },
+    {
+        'jose-elias-alvarez/null-ls.nvim', -- for formatters and linters
+        lazy = true
+    },
+    {
+        --Java Debugging plugin---
+        'mfussenegger/nvim-jdtls',
+        lazy = true
+    },
     -- debugging
-    'mfussenegger/nvim-dap',
-    'rcarriga/nvim-dap-ui',
-    'theHamsta/nvim-dap-virtual-text',
-    'nvim-telescope/telescope-dap.nvim'
+    {
+        'mfussenegger/nvim-dap',
+        lazy = true
+    },
+    {
+        'rcarriga/nvim-dap-ui',
+        lazy = true
+    },
+    {
+        'theHamsta/nvim-dap-virtual-text',
+        lazy = true
+    },
+    {
+        'nvim-telescope/telescope-dap.nvim',
+        lazy = true
+    }
 }
 
 -- Initialize lazy.nvim with your plugins
