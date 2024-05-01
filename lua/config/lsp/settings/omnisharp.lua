f1a6b4a0e5bf41ce00af8e63d8ec1d069d9dd24e
Mandeep Smagh <mandeep.smagh@outlook.com>
2024-05-01 22:13:51 +0800

    added lua and omnisharp settings

---
+++ lua/config/lsp/settings/omnisharp.lua
@@ -0,0 +1,7 @@
+return {
+    enable_roslyn_analysers = true,
+    enable_import_completion = true,
+    organize_imports_on_format = true,
+    enable_decompilation_support = true,
+    filetypes = { 'cs', 'vb', 'csproj', 'sln', 'slnx', 'props', 'csx', 'targets' }
+}
