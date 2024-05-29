04f19a068fbb4ad745f3e72d5b1ea48d4b1f0dce
Mandeep Smagh <mandeep.smagh@outlook.com>
2024-05-29 11:20:01 +0800

    changes to plugin

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -56,7 +56,7 @@ local plugins = {
                     comment_line = "<Leader>/",
                 },
             })
-            require("mini.sessions").setup({})
+            require("mini.sessions").setup()
         end,
     },
     {
