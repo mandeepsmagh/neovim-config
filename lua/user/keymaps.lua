bd6f12e0649b388267a8f4b95cb9954c33b948d9
Mandeep Smagh <mandeep.smagh@outlook.com>
2025-01-18 22:26:04 +0800

    keymaps commented to use builtin keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -7,19 +7,19 @@ local function map(mode, lhs, rhs, extra_opts)
     keymap(mode, lhs, rhs, options)
 end
 -------------------- MAPPINGS ------------------------------
-map("n", "<Space>", "") -- space as leader
-vim.g.mapleader = " " -- space as leader
-vim.g.maplocalleader = " " -- space as local leader
-map("n", "zz", ":up<CR>") -- save file
-map("", "<leader>c", '"+y') -- Copy to clipboard in normal, visual, select and operator modes
+map("n", "<Space>", "")          -- space as leader
+vim.g.mapleader = " "            -- space as leader
+vim.g.maplocalleader = " "       -- space as local leader
+map("n", "zz", ":up<CR>")        -- save file
+map("", "<leader>c", '"+y')      -- Copy to clipboard in normal, visual, select and operator modes
 map("n", "<leader>z", ":up<CR>") -- Save file
 map("i", "<C-u>", "<C-g>u<C-u>") -- Make <C-u> undo-friendly
 map("i", "<C-w>", "<C-g>u<C-w>") -- Make <C-w> undo-friendly
-map("n", "qq", ":q!<CR>") -- quit without saving
-map("n", "QQ", ":qa!<CR>") -- quit all without saving
-map("i", "jj", "<Esc>") -- Escape key mapped to jj
-map("n", ";", ":") -- Command mode
-map("n", "<leader>a", "ggVG") -- select All
+map("n", "qq", ":q!<CR>")        -- quit without saving
+map("n", "QQ", ":qa!<CR>")       -- quit all without saving
+map("i", "jj", "<Esc>")          -- Escape key mapped to jj
+map("n", ";", ":")               -- Command mode
+map("n", "<leader>a", "ggVG")    -- select All
 
 -- <Tab> to navigate the completion menu
 map("i", "<S-Tab>", 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', {
@@ -58,10 +58,10 @@ map("v", "<", "<gv")
 map("v", ">", ">gv")
 
 map("n", "<ESC><ESC>", "<cmd>noh<CR>") -- Clear highlights
-map("n", "<leader>o", "m`o<Esc>``") -- Insert a newline in normal mode
+map("n", "<leader>o", "m`o<Esc>``")    -- Insert a newline in normal mode
 
 ---nvim-tree-mappings-------------------
-map("n", "<leader>n", ":NvimTreeToggle<CR>") -- open/close
+map("n", "<leader>n", ":NvimTreeToggle<CR>")  -- open/close
 map("n", "<leader>r", ":NvimTreeRefresh<CR>") -- refresh
 
 -- Telescope-----------------------------
@@ -72,24 +72,24 @@ map("n", "<leader>gs", "<cmd>Telescope git_status<CR>")
 map("n", "<leader>dg", "<cmd>Telescope diagnostics<CR>")
 
 -- Split screen
-map("n", "<leader>s", "<cmd>split<CR>") -- horizontal split
+map("n", "<leader>s", "<cmd>split<CR>")  -- horizontal split
 map("n", "<leader>v", "<cmd>vsplit<CR>") -- vertical split
-map("n", "<C-h>", "<C-w>h") -- move to left
-map("n", "<C-j>", "<C-w>j") -- move to below
-map("n", "<C-k>", "<C-w>k") -- move to above
-map("n", "<C-l>", "<C-w>l") -- move to right
+map("n", "<C-h>", "<C-w>h")              -- move to left
+map("n", "<C-j>", "<C-w>j")              -- move to below
+map("n", "<C-k>", "<C-w>k")              -- move to above
+map("n", "<C-l>", "<C-w>l")              -- move to right
 
 -- LSP
 map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
 map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
-map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+-- map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
 map("n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>")
 map("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
 map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
 map("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<CR>", { desc = "Rename current symbol" })
 map("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-map("n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>')
-map("n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>')
+-- map("n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>')
+-- map("n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>')
 map("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>")
 map("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>")
 
