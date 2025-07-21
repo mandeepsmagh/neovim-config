999c5ac19ea74f4868797a9d3ee7713576eb41af
Mandeep Smagh <mandeep.smagh@outlook.com>
2025-07-21 21:49:37 +0800

    updated lsp.lua

--- lua/user/plugins/lsp.lua
+++ lua/user/plugins/lsp.lua
@@ -140,28 +140,48 @@ return {
                     on_attach = on_attach,
                 }
 
-                vim.lsp.config.omnisharp = {
-                    cmd = { "omnisharp" },
-                    filetypes = { "cs" },
-                    root_markers = { "*.sln", "*.csproj", ".git" },
-                    settings = {
-                        FormattingOptions = {
-                            EnableEditorConfigSupport = true,
-                            OrganizeImports = true,
+                vim.lsp.config.omnisharp = (function()
+                    local sysname = vim.loop.os_uname().sysname
+                    local dotnet_root = os.getenv("DOTNET_ROOT")
+
+                    if not dotnet_root then
+                        if sysname == "Darwin" then
+                            dotnet_root = "/usr/local/share/dotnet"
+                        elseif sysname == "Linux" then
+                            dotnet_root = "/usr/share/dotnet"
+                        elseif sysname == "Windows_NT" then
+                            dotnet_root = "C:\\Program Files\\dotnet"
+                        end
+                    end
+
+                    vim.env.DOTNET_ROOT = dotnet_root
+
+                    return {
+                        cmd = { "omnisharp" },
+                        filetypes = { "cs" },
+                        root_markers = { "*.sln", "*.csproj", ".git" },
+                        settings = {
+                            FormattingOptions = {
+                                EnableEditorConfigSupport = true,
+                                OrganizeImports = true,
+                            },
+                            RoslynExtensionsOptions = {
+                                EnableAnalyzersSupport = true,
+                                EnableImportCompletion = true,
+                            },
                         },
-                        RoslynExtensionsOptions = {
-                            EnableAnalyzersSupport = true,
-                            EnableImportCompletion = true,
+                        capabilities = capabilities,
+                        on_attach = on_attach,
+                        handlers = {
+                            ["textDocument/definition"] = function(...)
+                                return require("omnisharp_extended").handler(...)
+                            end,
                         },
-                    },
-                    capabilities = capabilities,
-                    on_attach = on_attach,
-                    handlers = {
-                        ["textDocument/definition"] = function(...)
-                            return require("omnisharp_extended").handler(...)
+                        on_new_config = function(new_config)
+                            new_config.cmd_env = { DOTNET_ROOT = dotnet_root }
                         end,
-                    },
-                }
+                    }
+                end)()
 
                 vim.lsp.config.jsonls = {
                     cmd = { "vscode-json-language-server", "--stdio" },
