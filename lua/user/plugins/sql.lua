15123cfd41b5bcf7a1d333c36e4a365051f4951f
Mandeep Smagh <mandeep.smagh@byrnecut.com.au>
2025-10-22 16:38:34 +0800

    updated max rows for mssql

--- lua/user/plugins/sql.lua
+++ lua/user/plugins/sql.lua
@@ -3,7 +3,8 @@ return {
     cond = vim.loop.os_uname().sysname == "Windows_NT",
     opts = {
         -- optional
-        keymap_prefix = "<leader>m"
+        keymap_prefix = "<leader>m",
+        max_rows = 1000,
     },
     -- optional
     dependencies = { "folke/which-key.nvim" }
