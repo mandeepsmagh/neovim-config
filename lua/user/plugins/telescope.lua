1b3ccc89f19885d4d0fdff2703f0f9bc2997c7dd
Mandeep Smagh <mandeep.smagh@outlook.com>
2025-11-01 16:52:01 +0800

    telescope update to find ignored files

--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -99,6 +99,7 @@ return {
                 pickers = {
                     find_files = {
                         hidden = true,
+                        no_ignore = true,
                         find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
                     },
                     live_grep = {
