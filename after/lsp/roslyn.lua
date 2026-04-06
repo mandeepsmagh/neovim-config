c2902cf638b482c13fd157895f3bfded620cd708
Mandeep Smagh <mandeep.smagh@outlook.com>
2026-04-06 19:46:51 +0800

    included docs for roslyn lsp server

--- after/lsp/roslyn.lua
+++ after/lsp/roslyn.lua
@@ -1,3 +1,18 @@
+--
+-- Roslyn Language Server for C#
+-- Install: dotnet tool install --global roslyn-language-server --prerelease
+-- Docs:    https://github.com/dotnet/roslyn/blob/main/src/LanguageServer/Microsoft.CodeAnalysis.LanguageServer/README.md
+--
+-- CMD OPTIONS (all optional, subject to change):
+--   --stdio                        Use stdin/stdout for communication (default: false)
+--   --pipe <name>                  Use named pipe for communication
+--   --autoLoadProjects             Auto-discover projects from workspace folders (default: false)
+--   --logLevel <level>             Trace|Debug|Information|Warning|Error|None (default: Information)
+--   --extensionLogDirectory <path> Directory for log files
+--   --extension <path>             Load extension assemblies, e.g. for Razor support
+--   --debug                        Launch debugger on startup (default: false)
+--   --telemetryLevel <level>       all|crash|error|off (default: off)
+
 return {
     cmd = {
         "roslyn-language-server",
