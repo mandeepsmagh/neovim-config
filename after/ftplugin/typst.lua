4850fdb289641a0bcd5fb0f488e8a17d7344681e
Mandeep Smagh <mandeep.smagh@outlook.com>
2026-09-20 15:10:15 +0800

    Simplify Typst indent ftplugin
    Set shiftwidth/softtabstop with vim.opt_local instead of a deferred
    re-apply.

--- after/ftplugin/typst.lua
+++ after/ftplugin/typst.lua
@@ -1,11 +1,2 @@
--- Neovim's bundled indent/typst.vim forces shiftwidth/softtabstop=2, and it
--- runs after this file. So set the values, then re-apply them once the indent
--- script has finished.
-local buf = vim.api.nvim_get_current_buf()
-vim.bo[buf].expandtab = true
-vim.bo[buf].shiftwidth = 4
-vim.bo[buf].softtabstop = 4
-vim.schedule(function()
-    vim.bo[buf].shiftwidth = 4
-    vim.bo[buf].softtabstop = 4
-end)
+vim.opt_local.shiftwidth = 4
+vim.opt_local.softtabstop = 4
